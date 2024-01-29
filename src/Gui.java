167a35b31f85f0ccfb3466fa5caeca08d884521a
Newton-Masese <newtonmasese23@gmail.com>
2024-01-29 19:56:17 +0000

    built up the Notepad GUI

--- src/Gui.java
+++ src/Gui.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Gui extends JFrame {
 
@@ -7,5 +8,49 @@ public class Gui extends JFrame {
         setSize(400,500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        addGuiComponents();
+    }
+
+    private void addGuiComponents(){
+        addToolBar();
+    }
+
+    private void addToolBar(){
+        JToolBar toolBar = new JToolBar();
+
+        //menu bar
+        JMenuBar menuBar = new JMenuBar();
+        toolBar.add(menuBar);
+
+        // add menus
+        menuBar.add(addMenuBar());
+
+        add(toolBar, BorderLayout.NORTH);
+    }
+    private JMenu addMenuBar(){
+        JMenu fileMenu = new JMenu("File");
+
+        // New - resets everything
+        JMenuItem newMenuItem = new JMenuItem("New");
+        fileMenu.add(newMenuItem);
+
+        // Open - open a text file
+        JMenuItem openMenuItem = new JMenuItem("Open");
+        fileMenu.add(openMenuItem);
+
+        // Save - save text to current file
+        JMenuItem saveMenuItem = new JMenuItem("Save");
+        fileMenu.add(saveMenuItem);
+
+        // Save As - creates file and saves text to the file
+        JMenuItem saveAsMenuItem = new JMenuItem("Save As");
+        fileMenu.add(saveAsMenuItem);
+
+        // exit - close program
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        fileMenu.add(exitMenuItem);
+
+        return fileMenu;
     }
 }
