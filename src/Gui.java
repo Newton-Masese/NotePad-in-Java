85dc10210ce03e0ee4f6f864523138e3f7068d8f
Newton-Masese <newtonmasese23@gmail.com>
2024-01-29 11:45:55 +0000

    First Release of Hello Word

---
+++ src/Gui.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Gui extends JFrame {
+
+    public Gui(){
+        super("NotePad");
+        setSize(400,500);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
